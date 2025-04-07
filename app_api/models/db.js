60a952ac414326daa9e5b5644b2530cbc850401a
SBMDM10 <mdm0597@hotmail.com>
2025-04-06 22:25:16 -0400

    Module 5 completed baseline

--- app_api/models/db.js
+++ app_api/models/db.js
@@ -2,22 +2,27 @@ const mongoose = require('mongoose');
 const host = process.env.DB_HOST || '127.0.0.1';
 const dbURI = `mongodb://${host}/travlr`;
 const readLine = require('readline');
+
 // Build the connection string and set the connection timeout.
 // timeout is in milliseconds.
 const connect = () => {
      setTimeout(() => mongoose.connect(dbURI, {
     }), 1000);
 }
+
 // Monitor connection events
 mongoose.connection.on('connected', () => {
     console.log(`Mongoose connected to ${dbURI}`);
 });
+
 mongoose.connection.on('error', err => {
     console.log('Mongoose connection error: ', err);
 });
+
 mongoose.connection.on('disconnected', () => {
     console.log('Mongoose disconnected');
 });
+
 // Windows specific listner
 if(process.platform === 'win32'){
     const r1 = readLine.createInterface({
